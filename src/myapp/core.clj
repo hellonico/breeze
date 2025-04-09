2722c9bd3b513aecaa726496972ca27ac9624ea7
Nicolas Modrzyk <hellonico@gmail.com>
2025-04-09 10:23:01 +0900

    streaming working

---
+++ src/myapp/core.clj
@@ -0,0 +1,68 @@
+(ns myapp.core
+ (:require
+  [org.httpkit.server :as httpkit]
+  [compojure.core :refer [GET POST defroutes]]
+  [compojure.route :as route]
+  [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+  [ring.middleware.params :refer [wrap-params]]
+  [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+  [pyjama.state :as p]
+  [ring.middleware.cors :refer [wrap-cors]]
+  [taoensso.sente :as sente]
+  [taoensso.sente.server-adapters.http-kit :refer [get-sch-adapter]]
+  [clojure.core.async :as async :refer [<! >! go-loop]])
+ (:gen-class))
+
+;; Setup Sente
+(let [{:keys [ch-recv send-fn connected-uids ajax-post-fn ajax-get-or-ws-handshake-fn]}
+      (sente/make-channel-socket-server!
+       (get-sch-adapter)
+       {:packer :edn :csrf-token-fn nil})]
+ (def ring-ajax-post ajax-post-fn)
+ (def ring-ajax-get-or-ws-handshake ajax-get-or-ws-handshake-fn)
+ (def ch-chsk ch-recv)
+ (def chsk-send! send-fn)
+ (def connected-uids connected-uids))
+
+(defn handle-msg! [{:keys [id ?data uid]}]
+ (when (= id :chat/start)
+  (let [state (atom (assoc ?data :processing true))
+        client-chan (fn [_ text] (chsk-send! uid [:chat/token text]))]
+   (p/ollama-chat state (partial client-chan nil))
+   ;; Wait in background until done, then notify client
+   (future
+    (loop []
+     (if (:processing @state)
+      (do (Thread/sleep 500) (recur))
+      ;; Processing done, notify client
+      (chsk-send! uid [:chat/done nil])))))))
+
+;; Message router
+(defn start-router! []
+ (go-loop []
+  (when-let [msg (<! ch-chsk)]
+   (handle-msg! msg)
+   (recur))))
+
+;; Routes
+(defroutes
+ app-routes
+ (GET "/chsk" req (ring-ajax-get-or-ws-handshake req))
+ (POST "/chsk" req (ring-ajax-post req))
+ (route/files "/")
+ (route/not-found "Not Found"))
+
+(def app
+ (-> app-routes
+     wrap-keyword-params
+     wrap-params
+     (wrap-cors :access-control-allow-origin [#"http://localhost:3000"])  ; adjust if needed
+     (wrap-defaults site-defaults)))
+
+(defonce server (atom nil))
+(defn start-server []
+ (reset! server (httpkit/run-server app {:port 3000}))
+ (start-router!)
+ (println "Server running at http://localhost:3000"))
+
+(defn -main [] (start-server))
---
+++ src/myapp/core.cljs
@@ -0,0 +1,106 @@
+(ns myapp.core
+ (:require
+  [reagent.core :as r]
+  [reagent.dom :as rdom]
+  [taoensso.sente :as sente]
+  [taoensso.sente.packers.transit :as sente-transit]))
+
+;; --- State ---
+(defonce app-state (r/atom {:messages   []
+                            :input      ""
+                            :streaming? false}))
+
+;; --- WebSocket Setup ---
+(let [{:keys [ch-recv send-fn state] :as chsk}
+      (sente/make-channel-socket-client!
+       "/chsk"
+       {:type   :auto
+        :packer (sente-transit/get-transit-packer)})]
+
+     (def chsk-send! send-fn)
+     (def ch-chsk ch-recv)
+     (def chsk-state state))
+
+;; --- Handle Streamed Tokens ---
+(defn handle-token [token]
+      (swap! app-state update-in [:messages (dec (count (:messages @app-state))) :content] str token))
+
+(defn handle-message! [{:keys [event]}]
+      (let [[id data] event]
+           (case id
+                 :chat/token (handle-token data)
+                 :chat/done  (swap! app-state assoc :streaming? false)
+                 nil)))
+
+(defonce stop-router! (atom nil))
+(defn start-router! []
+      (reset! stop-router!
+              (sente/start-client-chsk-router! ch-chsk handle-message!)))
+
+;; --- Scroll to bottom on update ---
+(defonce scroll-ref (atom nil))
+(defn scroll-to-bottom! []
+      (when-let [el @scroll-ref]
+                (set! (.-scrollTop el) (.-scrollHeight el))))
+
+(add-watch app-state :scroll
+           (fn [_ _ _ _] (js/setTimeout scroll-to-bottom! 50)))
+
+;; --- Send Prompt ---
+(defn send-prompt! []
+      (let [input (:input @app-state)]
+           (when (not (clojure.string/blank? input))
+                 (swap! app-state update :messages conj {:role :user :content input})
+                 (swap! app-state update :messages conj {:role :assistant :content ""})
+                 (swap! app-state assoc :input "" :streaming? true)
+                 (chsk-send!
+                  [:chat/start
+                   {:url      "http://localhost:11434"
+                    :model    "llama3.2"
+                    :messages [{:role :user :content input}]}]))))
+
+;; --- Chat Bubble ---
+(defn message-bubble [{:keys [role content]}]
+      [:div {:class (str "message "
+                         (case role
+                               :user "message-user"
+                               :assistant "message-assistant"))
+             :style {:align-self (if (= role :user) "flex-end" "flex-start")}}
+       content])
+
+;; --- Input Box ---
+(defn input-box []
+      (let [streaming? (:streaming? @app-state)]
+           [:div.field.has-addons.mt-4
+            [:div.control.is-expanded
+             [:input.input {:type        "text"
+                            :placeholder "Type a message..."
+                            :value       (:input @app-state)
+                            :on-change   #(swap! app-state assoc :input (-> % .-target .-value))
+                            :on-key-down #(when (and (= (.-key %) "Enter") (not streaming?))
+                                                (send-prompt!))
+                            :disabled    streaming?}]]
+            [:div.control
+             [:button.button.is-link {:on-click send-prompt!
+                                      :disabled streaming?}
+              "Send"]]]))
+
+;; --- Main App Component ---
+(defn app []
+      (r/create-class
+       {:component-did-mount start-router!
+        :reagent-render
+        (fn []
+            [:div
+             [:h1.title "Streaming Chat"]
+             [:div#chat-box.chat-container
+              {:ref #(reset! scroll-ref %)}
+              (for [[i msg] (map-indexed vector (:messages @app-state))]
+                   ^{:key i} [message-bubble msg])]
+             (when (:streaming? @app-state)
+                   [:p.has-text-grey "Assistant is typing..."])
+             [input-box]])}))
+
+;; --- Mount ---
+(defn ^:export init []
+      (rdom/render [app] (.getElementById js/document "app")))
